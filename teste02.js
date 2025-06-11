cbecfa72333243ef127b6c9b5a00572d35f51bf3
aiisShock <hobertcefas@gmail.com>
2025-06-11 17:16:39 -0300

    Add files via upload

--- teste02.js
+++ teste02.js
@@ -1,1 +1,16 @@
-var nomeUsuario = 'Robert';
+var pessoaUsuario = 'Robert';
+console.log(pessoaUsuario);
+var idadeUsuario =  "20";
+console.log(idadeUsuario);
+const dataUsuario = "16"; // let (ira mudar o valor no futuro) e const (Não poderar mudar o valor)
+let anoUsuario = 2005;
+    anoUsuario = anoUsuario + 1; //Pode calcular uma varivel 
+console.log(anoUsuario);
+let nascimentoUsuario = 2005 //Mesmas função do de cima mais simplificada e pratica na minha opinião
+    nascimentoUsuario += 1;
+console.log(nascimentoUsuario);
+let a = 30;
+a -= 12; //o mesmo que a = a - 12
+console.log(a);
+let alpha = 35;
+alpha++; // Ainda mais simplificada <3
--- testeJS.js
+++ testeJS.js
@@ -4,5 +4,7 @@ console.log('Robert' + ' Cefas'); //Concatenação
 console.log("Visionario".length); //Contagem de Letras
 console.log(Math.random()); //Numero aleatorio entre 0 e 1
 console.log(Math.random() * 50); //Numero aleatoria entre 0 e 50
+console.log(Math.floor(Math.random() * 50)); // Deixa o nome Inteiro
+
 
 
