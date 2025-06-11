1e492a5069d37381425ea6fc03a570dd25f45a72
aiisShock <hobertcefas@gmail.com>
2025-06-11 16:53:27 -0300

    Add files via upload

---
+++ teste02.js
@@ -0,0 +1,1 @@
+var nomeUsuario = 'Robert';
---
+++ testeJS.js
@@ -0,0 +1,8 @@
+console.log('robert'.toUpperCase()); //Maiusculo    
+console.log(300-100); //Subtração
+console.log('Robert' + ' Cefas'); //Concatenação
+console.log("Visionario".length); //Contagem de Letras
+console.log(Math.random()); //Numero aleatorio entre 0 e 1
+console.log(Math.random() * 50); //Numero aleatoria entre 0 e 50
+
+
